7b79897bc17a8c48a4a7b6d4c51312e472daeecd
htd <htd1702@gmail.com>
2021-12-15 16:23:23 +0700

    update category for admin

--- ShopProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/FileUploadUtil.java
+++ ShopProject/ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/FileUploadUtil.java
@@ -1,6 +1,5 @@
 package com.shop.admin;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -8,10 +7,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.multipart.MultipartFile;
 
 public class FileUploadUtil {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(FileUploadUtil.class);
 	public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile) throws IOException {
 		Path uploadPath = Paths.get(uploadDir);
 		if (!Files.exists(uploadPath)) {
@@ -33,12 +34,14 @@ public class FileUploadUtil {
 					try {
 						Files.delete(file);
 					} catch (IOException ex) {
-						System.out.println("Could not delete file: " + file);
+//						System.out.println("Could not delete file: " + file);
+						LOGGER.error("Could not delete file: " + file);
 					}
 				}
 			});
 		}catch (IOException ex) {
-			System.out.println("Could not list didectory: " + dirPath);
+//			System.out.println("Could not list directory: " + dirPath);
+			LOGGER.error("Could not list directory: " + dirPath);
 		}
 	}
 }
